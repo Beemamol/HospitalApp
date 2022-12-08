7c5a9eda5aa9413b9f56a378bdfbc5368f842d38
beemamolshaji <beemamolshaji98@gmail.com>
2022-12-08 19:58:10 +0530

    viewall

---
+++ src/app/patient-viewall/patient-viewall.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-viewall',
+  templateUrl: './patient-viewall.component.html',
+  styleUrls: ['./patient-viewall.component.css']
+})
+export class PatientViewallComponent {
+
+}
